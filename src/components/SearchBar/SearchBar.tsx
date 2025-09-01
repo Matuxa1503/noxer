9e4762be6976f1dfc0cf9c6281040b64cc2c09e7
Matuxa1503 <muravskiy.03@mail.ru>
2025-09-01 23:31:24 +0300

    feat: added btn for SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,5 +2,11 @@ import { FC } from 'react';
 import s from './SearchBar.module.css';
 
 export const SearchBar: FC = () => {
-  return <input type="text" className={s.input} placeholder="Найти товары" />;
+  return (
+    <div className={s.wrapper}>
+      <input type="text" className={s.input} placeholder="Найти товары" />
+      {/* logic for visibility btn  */}
+      {/* <button className={s.btn}>Перейти</button> */}
+    </div>
+  );
 };
